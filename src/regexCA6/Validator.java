b26d685b771dd78b944db0417fe9a1b4a9c4b89e
blank789 <40401992+blank789@users.noreply.github.com>
2021-06-01 15:14:33 -0400

    created variables, created object of Scanner class, user enters username into program

--- src/regexCA6/Validator.java
+++ src/regexCA6/Validator.java
@@ -1,13 +1,25 @@
 package regexCA6;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Validator {
 
-	public static void main (String[] args){
+	public static void main (String[] args) throws FileNotFoundException{
+		
+		Scanner scnr =new Scanner(System.in);
+		
+		//variables
+		String username;
+		String password;
+		boolean foundUser = false;
+		boolean foundPass = false;
 		
 		
-		
-	
+		//user enters username :: Username must: 1. Contain 6 to 8 characters 2. Include letters and numbers only 3. Start with a capital letter 4. End with a number
+		System.out.println("Enter username: ");
+		username = scnr.nextLine();
 		    
 		    
 		    
